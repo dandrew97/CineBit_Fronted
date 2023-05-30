2543435891b26fd00ec465b0b1b6502fd65ec593
StivenTorres17 <stiventorresbusiness@gmail.com>
2023-05-29 23:50:09 -0500

    Co-authored-by: Andrés Muñoz kaiin69@users.noreply.github.com
    En conjunto con el desarrollador Andrés Muñoz, se  ha implementado exitosamente los componentes de la barra de navegación y la navegación lateral. Ambos componentes funcionan perfectamente. La única tarea pendiente es definir los estilos, pero esto se trabajará de forma individual.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//! Importaciones paginas
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { RegistrarComponent } from './paginas/registrar/registrar.component';
+import { IniciarSesionComponent } from './paginas/iniciar-sesion/iniciar-sesion.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { FavoritosComponent } from './paginas/favoritos/favoritos.component';
+import { ExploraComponent } from './paginas/explora/explora.component';
+import { Top5Component } from './paginas/top5/top5.component';
+import { PerfilComponent } from './paginas/perfil/perfil.component';
+//! Importaciones Subpaginas
+import { CartoonsComponent } from './subpaginas/cartoons/cartoons.component';
+import { ColeccionesComponent } from './subpaginas/colecciones/colecciones.component';
+import { DocumentalesComponent } from './subpaginas/documentales/documentales.component';
+import { GenerosComponent } from './subpaginas/generos/generos.component';
+import { PeliculasComponent } from './subpaginas/peliculas/peliculas.component';
+import { SeriesComponent } from './subpaginas/series/series.component';
+//! Importación 404 
+import { NotFoundComponent } from './paginas/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'registrar', component:RegistrarComponent},
+  { path: 'iniciar-sesion', component:IniciarSesionComponent},
+  { path: 'home', component:HomeComponent},
+  { path: 'favoritos', component:FavoritosComponent},
+  { path: 'explora', component:ExploraComponent},
+  { path: 'top5', component:Top5Component},
+  { path: 'perfil', component:PerfilComponent},
+
+  { path: 'cartoons', component:CartoonsComponent},
+  { path: 'colecciones', component:ColeccionesComponent},
+  { path: 'documentales', component:DocumentalesComponent},
+  { path: 'generos', component:GenerosComponent},
+  { path: 'peliculas', component:PeliculasComponent},
+  { path: 'series', component:SeriesComponent},
+  
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: 'NotFoundComponent' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
