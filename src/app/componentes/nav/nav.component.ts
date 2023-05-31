64109d524e69f8fc605bac5dab12a29b3706bbaa
Andres <eamu.1992@gmail.com>
2023-05-31 14:33:47 -0500

    feat: :sparkles: Corregidos errores y adjuntados con éxito el sidebar y la barra de navegación
    Este commit aborda varios problemas e introduce los siguientes cambios:  Se corrigieron los errores encontrados en toda la aplicación. Se adjuntaron con éxito el sidebar y la barra de navegación. Se preparó el color de fondo para todas las páginas. Se realizaron otras correcciones y mejoras. Por favor, revisa y realiza el merge de este commit.

--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+    //* Seleccionar elementos del DOM
+    const input = document.querySelector('.input') as HTMLInputElement;
+    const icon = document.querySelector('.icon') as HTMLButtonElement;
 
+    //? Evento de clic en el ícono para enfocar el input
+    icon.addEventListener('click', () => {
+      input.focus();
+    });
+
+    //? Evento de enfoque del input para agregar la clase 'focused' al ícono
+    input.addEventListener('focus', () => {
+      icon.classList.add('focused');
+    });
+
+    //? Evento de desenfoque del input para quitar la clase 'focused' al ícono si no hay valor en el input
+    input.addEventListener('blur', () => {
+      if (input.value === '') {
+        icon.classList.remove('focused');
+      }
+    });
+  }
 }
