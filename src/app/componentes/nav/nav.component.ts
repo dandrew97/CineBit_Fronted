bcca5b557380f0efbd07333934710478cb4b105e
StivenTorres17 <stiventorresbusiness@gmail.com>
2023-05-31 15:00:58 -0500

    Revert "feat: :sparkles: Se  ha implementado exitosamente los componentes de la barra de navegación y la navegación lateral."
    This reverts commit f35525b5c38ef5d10f09cdd7495c8b938e623e7c.

--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -1,31 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss'],
+  styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {
-    //* Seleccionar elementos del DOM
-    const input = document.querySelector('.input') as HTMLInputElement;
-    const icon = document.querySelector('.icon') as HTMLButtonElement;
+export class NavComponent {
 
-    //? Evento de clic en el ícono para enfocar el input
-    icon.addEventListener('click', () => {
-      input.focus();
-    });
-
-    //? Evento de enfoque del input para agregar la clase 'focused' al ícono
-    input.addEventListener('focus', () => {
-      icon.classList.add('focused');
-    });
-
-    //? Evento de desenfoque del input para quitar la clase 'focused' al ícono si no hay valor en el input
-    input.addEventListener('blur', () => {
-      if (input.value === '') {
-        icon.classList.remove('focused');
-      }
-    });
-  }
 }
