5b077f72f948b1e75d8b975536e89a021e062686
dandrew97 <dcampos823@gmail.com>
2023-06-09 15:42:31 -0500

    style: :art: Se cambian estilos y se crean componentes
    Se cambia el estilo de los sliders y se crean unos componentes en la pagina colecciones

--- src/app/componentes/slider/slider2/slider2.component.ts
+++ src/app/componentes/slider/slider2/slider2.component.ts
@@ -5,17 +5,45 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   templateUrl: './slider2.component.html',
   styleUrls: ['./slider2.component.scss']
 })
-export class Slider2Component implements OnInit {
-  @ViewChild('contenedorCarrusel') contenedorCarrusel!: ElementRef;
+export class Slider2Component {
+  @ViewChild('carrusel') carrusel!: ElementRef;
+  @ViewChild('flechaIzquierda') flechaIzquierda!: ElementRef;
+  @ViewChild('flechaDerecha') flechaDerecha!: ElementRef;
   
   constructor() {}
 
   ngOnInit(): void {
-    const fila: HTMLElement = this.contenedorCarrusel.nativeElement;
-    const flechaDerecha = fila.querySelector('.flechaDerecha');
-
-    flechaDerecha?.addEventListener('click', () => {
-      fila.scrollLeft += fila.offsetWidth
-    })
+    this.flechaIzq();
+    this.flechaDer();
   }
+
+  flechaIzq() {
+    const carruselElement = this.carrusel.nativeElement;
+    const flechaIzquierdaElement = this.flechaIzquierda.nativeElement;
+    
+
+    flechaIzquierdaElement.addEventListener('click', () => {
+      carruselElement.scrollLeft -= carruselElement.offsetWidth;
+    });
+  };
+
+  flechaDer() {
+    const carruselElement = this.carrusel.nativeElement;
+    const flechaDerechaElement = this.flechaDerecha.nativeElement;
+
+    flechaDerechaElement.addEventListener('click', () => {
+    carruselElement.scrollLeft += carruselElement.offsetWidth;
+    });
+  }
+    
+  
+
+  // ngOnInit(): void {
+  //   const fila: HTMLElement = this.contenedorCarrusel.nativeElement;
+  //   const flechaDerecha = fila.querySelector('.flechaDerecha');
+
+  //   flechaDerecha?.addEventListener('click', () => {
+  //     fila.scrollLeft += fila.offsetWidth
+  //   })
+  // }
 }
