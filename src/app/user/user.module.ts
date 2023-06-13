3b67e00c52611227d6a0e89d94fba8bbbdf35d3f
dandrew97 <dcampos823@gmail.com>
2023-06-13 01:43:24 -0500

    feat: :construction: Se crea la pagina de colecciones y se modifica un poco la estructura del user.module para corregir un error en las rutas
    Se ha creado una nueva página llamada "colecciones" que ofrece emocionante contenido de la franquicia de películas.  Se presenta una tarjeta visualmente atractiva que muestra el logotipo de Marvel y una imagen representativa del Capitán América, uno de los personajes destacados de la franquicia.  Además, se incluye un contenedor de películas de Marvel que contiene varias tarjetas de películas. Cada tarjeta muestra una imagen de la película y proporciona botones para reproducir el tráiler y obtener más información sobre cada película.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,23 +1,11 @@
-import { NgModule } from '@angular/core'; //TODO Importación del módulo NgModule para la definición del módulo principal
-import { BrowserModule } from '@angular/platform-browser'; //TODO Importación del módulo BrowserModule para la compatibilidad con el navegador
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //TODO Importación del módulo BrowserAnimationsModule para habilitar animaciones
-import { MatFormFieldModule } from '@angular/material/form-field'; //TODO Importación del módulo MatFormFieldModule para utilizar campos de formulario de Material
-import { MatInputModule } from '@angular/material/input'; //TODO Importación del módulo MatInputModule para utilizar campos de entrada de Material
-import { MatSelectModule } from '@angular/material/select'; //TODO Importación del módulo MatSelectModule para utilizar selectores de Material
-import { ReactiveFormsModule } from '@angular/forms'; //TODO Importación del módulo ReactiveFormsModule para trabajar con formularios reactivos
-import { MatIconModule } from '@angular/material/icon'; //TODO Importación del módulo MatIconModule para utilizar iconos de Material
-import { MatChipsModule } from '@angular/material/chips'; //TODO Importación del módulo MatChipsModule para utilizar chips de Material
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { CreateComponent } from './create/create.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   
@@ -30,19 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     UserRoutingModule,
-    FormsModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    ReactiveFormsModule,
-    MatIconModule,
-    MatChipsModule,
-    MatCardModule,
-    MatCheckboxModule,
-    FormsModule,
-    HttpClientModule
+    FormsModule
   ]
 })
 
