3dc35e19fdca07c287e60554043f108d62e1f28d
StivenTorres17 <stiventorresbusiness@gmail.com>
2023-06-16 03:55:49 -0500

    fix: :goal_net: Solucionado error de creación, inicio de sesión y perfil
    Se solucionó el error de usuario que impedía el correcto funcionamiento de la creación, inicio de sesión y perfil. Fueron necesarios más de 10 commits y 6 horas de trabajo continuo de un equipo de 5 personas, pero finalmente se logró resolver el problema.  Además, se añadieron nuevas funcionalidades, como la redirección después de iniciar sesión. Queda pendiente trabajar en los estilos.  Estas son las tareas completadas:  Solución del error de creación de usuario Solución del error de inicio de sesión Solución del error de perfil de usuario Añadida redirección después de iniciar sesión

--- src/app/services/userservices.service.ts
+++ src/app/services/userservices.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class UserservicesService {
   private apiUrl = "http://localhost:3000/users";
   private userData!:string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router:Router) { }
 
   login(email: string, password: string): void {
     const loginUrl = `${this.apiUrl}/login`;
@@ -24,6 +25,7 @@ export class UserservicesService {
           localStorage.setItem("token", response.token);
           this.userEmail=formData.email;
           console.log("este es el email login", this.userEmail);
+          this.router.navigate(['/user/profile']);
         },
         (error) => {
           if(error instanceof HttpErrorResponse){
